398cbf74a684946181a0626b801d27a406a8806d
Riley Moore <rilemoore@gmail.com>
2019-11-30 00:20:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
+    puts params
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
@@ -31,12 +32,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    @user = User.find_by(email: params[:email], password: params[:password])
-    if @user
-      session[:user_id] = @user.id
-      redirect '/users/home'
-    end
-    redirect '/sessions/login'
+    @user = User.find_by(email: params["email"], password: params["password"])
+    session[:user_id] = @user.id
+    
+    redirect '/users/home'
   end
 
   get '/sessions/logout' do
